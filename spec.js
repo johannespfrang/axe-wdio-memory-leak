c3131b405d8543d07d849dcda24242e47c9b7047
Johannes Pfrang <159568713+johannespfrang@users.noreply.github.com>
2024-07-22 14:04:35 +0200

    Also log memory usage to console during run

--- spec.js
+++ spec.js
@@ -5,10 +5,15 @@ describe("Accessibility Test", () => {
 	it("should not leak memory", async () => {
 		const axe = new AxeBuilder({ client: browser });
 		await browser.url("https://example.com/");
-    console.info("Attach DevTools now.")
-    await browser.pause(10000)
-		for (let i = 0; i < 20; i++) {
+		console.info("Attach DevTools now.");
+		await browser.pause(5000);
+		console.info("Starting...");
+		for (let i = 0; i < 50; i++) {
 			await axe.analyze();
+			const heapSize = process.memoryUsage().heapUsed / 1024 / 1024;
+			console.log(`Heap allocated ${heapSize} MB`);
 		}
+		console.info("Done.");
+		await browser.debug();
 	});
 });
